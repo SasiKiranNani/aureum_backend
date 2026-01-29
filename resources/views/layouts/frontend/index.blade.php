7299773c679264c9bfeeeaaf0dc57ca61b857f62
SasiKiranNani <psk123153@gmail.com>
2026-01-30 00:42:35 +0530

    modified the color scheme css

--- resources/views/layouts/frontend/index.blade.php
+++ resources/views/layouts/frontend/index.blade.php
@@ -18,7 +18,7 @@
     <link href="{{ asset('frontend/css/custom.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('frontend/css/auth-animated.css') }}" rel="stylesheet" type="text/css">
     <!-- color scheme -->
-    <link id="colors" href="{{ 'frontend/css/colors/scheme-01.css' }}" rel="stylesheet" type="text/css">
+    <link id="colors" href="{{ asset('frontend/css/colors/scheme-01.css') }}" rel="stylesheet" type="text/css">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
