33bf566372467e2f47c4ed02dbf0e2b8e710310f
SasiKiranNani <psk123153@gmail.com>
2026-01-31 15:29:45 +0530

    changes made for judge dashboard

--- app/Providers/Filament/JudgePanelProvider.php
+++ app/Providers/Filament/JudgePanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use Filament\Http\Middleware\Authenticate;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -54,6 +55,9 @@ class JudgePanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                FilamentShieldPlugin::make(),
             ])
             ->sidebarCollapsibleOnDesktop()
             ->maxContentWidth('full')
